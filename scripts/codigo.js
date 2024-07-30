949a766ed1b17ff09364b4cd31b93c0487ed83d4
kaely <kaely.olag01@gmai.com>
2024-07-29 19:24:15 -0600

    Controles de Flujo

---
+++ scripts/codigo.js
@@ -0,0 +1,34 @@
+/*If simple 
+let edad = 19;
+if(edad >= 18){
+    console.log("Es mayor de edad");
+}
+//If - else
+let x = 10;
+if(x > 0){
+    console.log("Esun numero positivo");
+}else{
+    console.log("El numero es negativo o 0");
+}
+//Else - if
+let hora = 11;
+if(hora < 12){
+    console.log("Buenos dias");
+}else if(hora < 19){
+    console.log("Buenas tardes");
+}else{
+    console.log("Buenas noches");
+}*/
+//While
+/*let i = 8;
+while(i <= 10){
+    console.log(i);
+    i += 1;
+} */
+//Do - while 
+let x = 5;
+do{
+    console.log(x);
+    x -= 1;
+
+}while(x > 1);
